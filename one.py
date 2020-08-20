7552619be0b8ba0bd82c19138549768c7b6bc975
Kevin <knphillips01@gmail.com>
2020-08-20 16:18:28 -0500

    first two code challenges

---
+++ one.py
@@ -0,0 +1,8 @@
+# Today's Coding Challenge:
+# Print out each element of the following array on a separate line
+my_list = ["Joe", "2", "Ted", "4.98", "14", "Sam", "void *", "42", "float", "pointers", "5006"]
+# You may use whatever programming language you'd like.
+# Verbalize your thought process as much as possible before writing any code.
+
+for x in my_list:
+  print(x);
---
+++ two.py
@@ -0,0 +1,15 @@
+# Print out all of the numbers in the following array that are divisible by 3:
+my_list = [85, 46, 27, 81, 94, 9, 27, 38, 43, 99, 37, 63, 31, 42, 14]
+# The expected output for the above input is:
+#   27
+#   81
+#   9
+#   27
+#   99
+#   63
+#   42
+# Verbalize your thought process as much as possible before writing any code. Run through #  the UPER problem solving framework while going through your thought process.
+for x in my_list:
+  if x % 3 == 0:
+    print(x);
+
